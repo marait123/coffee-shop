7bfe9bd7e8ce2bc63f255c8a6e4a82a5b8c45643
marait123 <max.ibrahim43@hotmail.com>
2020-09-20 02:00:39 +0200

    revised pep8 and deleted all todos except the drop_db todo

--- backend/src/api.py
+++ backend/src/api.py
@@ -22,15 +22,6 @@ CORS(app)
 # db_drop_and_create_all()
 
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-        returns status code 200 and json {"success": True, "drinks": drinks}
-        where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 
 
 @app.route('/drinks', methods=["GET"])
@@ -39,16 +30,6 @@ def get_drinks():
     drinks = [drink.short() for drink in drinks]
     return jsonify(success=True, drinks=drinks), 200
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-        returns status code 200 and json {"success": True, "drinks": drinks}
-        where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
-
 
 @app.route('/drinks-detail', methods=["GET"])
 @requires_auth(["get:drinks-detail"])
@@ -56,18 +37,6 @@ def get_detail_drinks(payload):
     drinks = Drink.query.all()
     drinks = [drink.long() for drink in drinks]
     return jsonify(success=True, drinks=drinks), 200
-
-
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-        returns status code 200 and json {"success": True, "drinks": drink}
-        where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 
 
 @app.route('/drinks', methods=["POST"])
@@ -97,20 +66,6 @@ def post_drinks(payload):
     return jsonify(success=True, drinks=drinks), 200
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-        returns status code 200 and json {"success": True, "drinks": drink}
-        where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
-
-
 @app.route('/drinks/<int:id>', methods=["PATCH"])
 @requires_auth(["patch:drinks"])
 def update_drink(payload, id):
@@ -131,19 +86,6 @@ def update_drink(payload, id):
         return jsonify(success=True, drinks=[drink.long()]), 200
     except:
         abort(400)
-
-
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-        returns status code 200 and json {"success": True, "delete": id}
-        where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
 
 
 @app.route('/drinks/<int:id>', methods=["DELETE"])
--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -10,10 +10,6 @@ ALGORITHMS = ['RS256']
 API_AUDIENCE = 'CafeApi'
 
 # AuthError Exception
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
 
 
 class AuthError(Exception):
@@ -23,15 +19,6 @@ class AuthError(Exception):
 
 
 # Auth Header
-
-'''
-@TODO implement get_token_auth_header() method
-    it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
-    return the token part of the header
-'''
 
 
 def get_token_auth_header():
@@ -66,20 +53,6 @@ def get_token_auth_header():
     token = parts[1]
     return token
 
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
-
-    it should raise an AuthError if permissions are not included in the payload
-    !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission
-    string is not in the payload permissions array
-    return true otherwise
-'''
-
-
 def check_permissions(permissions, payload):
     if 'permissions' not in payload:
         return False
@@ -87,19 +60,6 @@ def check_permissions(permissions, payload):
         if permission not in payload['permissions']:
             return False
     return True
-'''
-@TODO implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
-
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
-
-'''
-
 
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
@@ -153,19 +113,6 @@ def verify_decode_jwt(token):
                 'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
             }, 400)
-'''
-@TODO implement @requires_auth(permission) decorator method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-
-    it should use the get_token_auth_header method to get the token.
-    it should use the verify_decode_jwt method to decode the jwt.
-    it should use the check_permissions method validate
-    claims and check the requested permission.
-    return the decorator which passes the decoded payload
-    to the decorated method.
-'''
-
 
 def requires_auth(permissions):
     def requires_auth_decorator(f):
